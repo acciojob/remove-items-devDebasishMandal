7e51720be66c253a36991a5fcd582e50f9b11af5
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-02-06 22:59:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let x= document.getElementById("btn");
+
+x.onclick = function(){
+	
+	let s=document.getElementById("colorSelect");
+	s.remove(s.selectedIndex);
+};
+
